63a0bde47a7ca01d4a2631999454656fb2715bf3
Mehmet Teker <teker.mehmed@gmail.com>
2020-04-12 21:57:06 +0300

    Dependency Injection Yapısı

---
+++ DesignPatterns/DependencyInjection/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProdcutDal());
+            productManager.Save();
+            Console.ReadLine();
+        }
+    }
+
+    interface IProductDal
+    {
+        void Save();
+    }
+
+    class EfProdcutDal : IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Save  With EF");
+        }
+    }
+
+    class NhProdcutDal : IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Save  With NH");
+        }
+    }
+
+    class ProductManager
+    {
+        private readonly IProductDal productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            this.productDal = productDal;
+        }
+        // Business Code
+        public void Save()
+        {
+            // ProdcutDal prodcutDal = new ProdcutDal(); Eski yapı bunun yerine yukarda consructor ın içinde hangi nesne ile 
+            // çalışacaksa orda set ettik.
+            productDal.Save();
+        }
+    }
+
+}
+
